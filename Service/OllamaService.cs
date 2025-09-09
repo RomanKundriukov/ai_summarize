83647329d695d08bd4a5a5e573136418e2a2ade7
Roman Kundriukov <r.kundriukov@web.de>
2025-09-09 08:02:28 +0200

    Ollama Servise Start

--- Service/OllamaService.cs
+++ Service/OllamaService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ai_summarize.Model;
+using OllamaSharp;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,70 @@ using System.Threading.Tasks;
 
 namespace ai_summarize.Service
 {
-    internal class OllamaService
+    internal class OllamaService : IDisposable
     {
+
+        private bool disposed = false;
+        private readonly Uri _uri;
+        private readonly OllamaApiClient _ollama;
+        internal OllamaService(Uri uri)
+        {
+            _uri = uri;
+            _ollama = new OllamaApiClient(_uri);
+        }
+
+        internal async Task<List<OllamaModels>> GetModelsAsync()
+        {
+            try
+            {
+                var models = await _ollama.ListLocalModelsAsync();
+
+                return models
+                    .Select(m => new OllamaModels { ModelName = m.Name })
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                Dispose();
+            }
+           
+        }
+
+        internal void ChatAsync()
+        {
+
+        }
+
+        #region Aufräumen
+        ~OllamaService()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+            if (disposing)
+            {
+                // managed freigeben
+                _ollama.Dispose();
+             
+            }
+            // unmanaged hier freigeben
+            disposed = true;
+        }
+        #endregion
+
     }
 }
--- ViewModel/HomePageViewModel.cs
+++ ViewModel/HomePageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using ai_summarize.Model;
+using ai_summarize.Service;
 using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.Generic;
@@ -12,16 +13,27 @@ namespace ai_summarize.ViewModel
     internal class HomePageViewModel
     {
         internal ObservableCollection<OllamaModels> models = new ObservableCollection<OllamaModels>();
+        private readonly OllamaService _ollama = new OllamaService(new Uri("http://localhost:11434"));
 
         internal HomePageViewModel() 
         {
             LoadModels();
+           
         }
-        private void LoadModels()
+        private async void LoadModels()
         {
-            models.Add(new OllamaModels { ModelName = "Model 1"});
-            models.Add(new OllamaModels { ModelName = "Model 2"});
-            models.Add(new OllamaModels { ModelName = "Model 3"});
+            List<OllamaModels> ollamaModels = await _ollama.GetModelsAsync();
+
+            if(ollamaModels.Count <= 0)
+                models.Add(new OllamaModels { ModelName = "You don´t have a Models." });
+            else
+            {
+                foreach (var item in ollamaModels)
+                {
+                    models.Add(new OllamaModels { ModelName = item.ModelName });
+                }
+            }    
+
             
         }
 
